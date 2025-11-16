8f30e211aca4f162723c4580f831501198a7b33a
Dylan Cancelliere <dycancel@gmail.com>
2025-11-16 13:04:04 -0500

    Remove career fair filter

--- src/routes/guide/index.tsx
+++ src/routes/guide/index.tsx
@@ -2,6 +2,6 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/guide/")({
     beforeLoad: () => {
-        throw redirect({ to: "/guide/data", search: { filters: [{ id: "tags", value: ["RIT Fall 2025 Career Fair"] }] } });
+        throw redirect({ to: "/guide/data" });
     },
 });
